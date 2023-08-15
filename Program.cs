00647466a2f3c0f68ac3c4cba4956687d97975ef
mohammedaladhary <caladhary@gmail.com>
2023-08-15 11:25:40 +0400

    figured out to get a_ as zero

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿using System.Diagnostics.Metrics;
+﻿
+using System.Diagnostics.Metrics;
 using System.Drawing;
 using System.Reflection;
 using System.Text.RegularExpressions;
-
 namespace PackageMeasurementConverterAPI
+
 {
     class Program
     {
@@ -15,33 +16,48 @@ namespace PackageMeasurementConverterAPI
             return char.ToLower(c) - 'a' + 1;
         }
 
-        static List<int> Decode(string encodedString)
+        static List<int> Package(string sequence)
         {
             List<int> results = new List<int>();
             int currentIndex = 0;
 
-            if (!encodedString.All(char.IsLetter))
+            while (currentIndex < sequence.Length)
             {
-                Console.WriteLine("Invalid input. Please enter only alphabetical characters.");
-            }
-
-            while (currentIndex < encodedString.Length)
-            {
-
-                int count = encodedString[currentIndex] - 'a' + 1;
+                int count = ConvertToNumber(sequence[currentIndex]);
                 currentIndex++;
 
-                string package = encodedString.Substring(currentIndex, count);
-                currentIndex += count;
-
-                int packageSum = 0;
-                foreach (char character in package)
+                if (count == 0) // Handle underscore as zero
                 {
-                    packageSum += character - 'a' + 1;
+                    Console.WriteLine("Invalid input. Please enter only alphabetical characters or underscores.");
+                    return null;
                 }
-                results.Add(packageSum);
 
+                if (currentIndex >= sequence.Length)
+                {
+                    Console.WriteLine("Invalid input sequence format.");
+                    return null;
+                }
 
+                if (count == 26) // Counter is 'z'
+                {
+                    int nextValue = ConvertToNumber(sequence[currentIndex]);
+                    int packageSum = count + nextValue;
+                    results.Add(packageSum);
+
+                    currentIndex++;
+                }
+                else
+                {
+                    string package = sequence.Substring(currentIndex, count);
+                    currentIndex += count;
+
+                    int packageSum = 0;
+                    foreach (char character in package)
+                    {
+                        packageSum += ConvertToNumber(character);
+                    }
+                    results.Add(packageSum);
+                }
             }
 
             return results;
@@ -55,15 +71,47 @@ namespace PackageMeasurementConverterAPI
 
             if (!sequence.All(c => char.IsLetter(c) || c == '_'))
             {
-                Console.WriteLine("Invalid input. Please enter only alphabetical characters or underscores.");
+                Console.WriteLine("Invalid input. Please enter only alphabetical characters with underscores/without underscores.");
                 return;
             }
             else
             {
-                List<int> decodedValues = Decode(sequence);
+                List<int> decodedValues = Package(sequence);
                 //Console.WriteLine($"\n(firstLetter: {sequence[0]}, no.of letters to combine: {decodedValues.Count})");
                 Console.WriteLine("\nThe Result is: " + "[" + string.Join(", ", decodedValues) + "]");
             }
         }
     }
 }
+
+//static List<int> Package(string sequence)
+//{
+//    List<int> results = new List<int>();
+//    int currentIndex = 0;
+
+//    //if (!sequence.All(char.IsLetter))
+//    //{
+//    //    Console.WriteLine("Invalid input. Please enter only alphabetical characters.");
+//    //}
+
+//    while (currentIndex < sequence.Length)
+//    {
+
+//        int count = sequence[currentIndex] - 'a' + 1;
+//        currentIndex++;
+
+//        string package = sequence.Substring(currentIndex, count);
+//        currentIndex += count;
+
+//        int packageSum = 0;
+//        foreach (char character in package)
+//        {
+//            packageSum += character - 'a' + 1;
+//        }
+//        results.Add(packageSum);
+
+
+//    }
+
+//    return results;
+//}
